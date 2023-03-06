629c5ac9784abe71091c07e1b559bb959f41e96f
Quentin <quentin01.bource@gmail.com>
2023-03-06 16:55:26 +0100

    Jour1

---
+++ Pikyou/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pikyou</title>
+</head>
+<body>
+    <head>
+        <img src="../resources/css/assets/logo_transparent.png" alt="logo">
+    </head>
+    
+</body>
+</html>
